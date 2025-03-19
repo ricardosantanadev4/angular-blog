fbee307ce264751358dfe941bf455ed817d2bf50
ricardosantanadev4 <ricardosantanadev4@gmail.com>
2025-03-19 15:09:14 -0300

    feat: Adicionas as configurações globais do projeto e criação dos components.

---
+++ src/app/components/featured-article/featured-article.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-featured-article',
+  imports: [],
+  templateUrl: './featured-article.component.html',
+  styleUrl: './featured-article.component.scss'
+})
+export class FeaturedArticleComponent {
+
+}
---
+++ src/app/components/podcast-list/podcast-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-podcast-list',
+  imports: [],
+  templateUrl: './podcast-list.component.html',
+  styleUrl: './podcast-list.component.scss'
+})
+export class PodcastListComponent {
+
+}
