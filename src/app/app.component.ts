f9d1d33bd401834b7a579c653a51e89db5143ed1
ricardosantanadev4 <ricardosantanadev4@gmail.com>
2025-03-20 19:03:03 -0300

    feat: Adicionadas as configurações do component. style: Adicionados os estilos.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FeaturedArticleComponent } from './components/featured-article/featured-article.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, HeaderComponent],
+  imports: [RouterOutlet, NavbarComponent, HeaderComponent,FeaturedArticleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
